04f8f395a1acbce72b9d9e6b41b4720d5144b642
Riley Shaw <rileyjshaw@gmail.com>
2021-07-04 15:37:59 -0400

    Add a % to daysalive

--- .daysalive.sh
+++ .daysalive.sh
@@ -2,7 +2,9 @@
 
 # requires coreutils (brew install)
 function daysalive() {
-	echo $(( ($(gdate -u +%s) - $(gdate -ud "1991-10-05" +%s)) /60/60/24 ));
+	local days=$(( ($(gdate -u +%s) - $(gdate -ud "1991-10-05" +%s)) /60/60/24 ));
+	# 80 seems generous, let’s go for it. adding 14610 rounds it.
+	echo "$days is $(( ($days * 100 + 14610) / 29220 ))%";
 }
 
 daysalive
